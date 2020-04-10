5f648a463438ecf918f0734fcfcf1c47a9ee3c11
Joshua Kim <jykim92@gmail.com>
2020-04-10 18:51:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,20 @@
 def nyc_pigeon_organizer(data)
-
+  new_hash = {}
+  data.each do |property, hash|
+    hash.each do |attritbute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
+        end
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end
 
-nyc_pigeon_organizer
